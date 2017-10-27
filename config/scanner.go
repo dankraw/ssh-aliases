81b7454b4f43d56e9f2f4a52c25fd6e845d694d6
Daniel Krawczyk <dankraw@gmail.com>
2017-10-27 21:55:11 +0200

    Config scanner

---
+++ config/scanner.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"io/ioutil"
+	"fmt"
+	"strings"
+)
+
+type Scanner struct {}
+
+func NewScanner() *Scanner {
+	return &Scanner{}
+}
+
+const hclExtension = ".hcl"
+
+func (s *Scanner) ScanDirectory(path string) ([]string, error) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+	hcls := []string{}
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), hclExtension) {
+			hcls = append(hcls, fmt.Sprintf("%s/%s", path, file.Name()))
+		}
+	}
+	return hcls, nil
+}
+
+func (s* Scanner) ReadFile(path string) ([]byte, error) {
+	return ioutil.ReadFile(path)
+}
---
+++ config/scanner_test.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShouldScanDir(t *testing.T) {
+	t.Parallel()
+	// given
+	scanner := NewScanner()
+
+	// when
+	hcls, err := scanner.ScanDirectory(fixtureDir)
+
+	// then
+	assert.NoError(t, err)
+	assert.Equal(t, []string{
+		"./test-fixtures/empty.hcl",
+		"./test-fixtures/example.hcl",
+	}, hcls)
+}

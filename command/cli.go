1716eb7b0b377cdafb4453983be8c1e8836a862d
Daniel Krawczyk <dankraw@gmail.com>
2017-11-07 22:02:25 +0100

    Improved CLI help

--- command/cli.go
+++ command/cli.go
@@ -34,11 +34,11 @@ func (c *CLI) ConfigureCLI() error {
 	app := cli.NewApp()
 	app.Version = c.version
 	app.Name = "ssh-aliases"
-	app.Usage = "Template driven SSH config generation"
+	app.Usage = "template driven ssh config generation"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:        "scan",
-			Usage:       "Path to scan for input HCL config files",
+			Name:        "scan, s",
+			Usage:       "input files dir",
 			Value:       filepath.Join(homeDir, sshAliasesDir),
 			Destination: &scanDir,
 		},
@@ -46,7 +46,7 @@ func (c *CLI) ConfigureCLI() error {
 	app.Commands = []cli.Command{{
 		Name:    "list",
 		Aliases: []string{"l"},
-		Usage:   "Prints to stdout the list of aliases and target hosts",
+		Usage:   "Prints the list of host definitions",
 		Action: func(ctx *cli.Context) error {
 			err := NewListCommand(os.Stdout).Execute(scanDir)
 			if err != nil {
@@ -57,22 +57,22 @@ func (c *CLI) ConfigureCLI() error {
 	}, {
 		Name:    "compile",
 		Aliases: []string{"c"},
-		Usage:   "Prints compiled SSH config file to stdout (default) or writes it to a file",
+		Usage:   "Prints compiled ssh config file or writes it to a file",
 		Flags: []cli.Flag{
 			cli.BoolFlag{
 				Name:        "save",
-				Usage:       "Write compilation output to file instead of printing to stdout",
+				Usage:       "write compilation output to file instead of printing to stdout",
 				Destination: &save,
 			},
 			cli.StringFlag{
 				Name:        "file",
-				Usage:       "Destination file path",
+				Usage:       "destination file path",
 				Destination: &file,
 				Value:       filepath.Join(homeDir, ".ssh", "config"),
 			},
 			cli.BoolFlag{
 				Name:        "force",
-				Usage:       "Overwrite existing file without confirmation",
+				Usage:       "overwrite existing file without confirmation",
 				Destination: &force,
 			},
 		},

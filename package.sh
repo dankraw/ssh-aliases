8d91295cb44611f62dbb75b7baa8ef9e2b702d05
Daniel Krawczyk <dankraw@gmail.com>
2025-08-16 20:43:26 +0200

    Add GitHub Actions workflow for building and releasing

--- package.sh
+++ package.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 0 ]
-  then
+if [ $# -eq 0 ]; then
     echo "No release version specified."
     exit 1
 fi
 
 VERSION=$1
-
 echo "Packaging ssh-aliases ${VERSION}"
 
 DIST="dist"
@@ -15,7 +13,7 @@ OUT_DIR="${DIST}/out"
 OUT_BINARY="${OUT_DIR}/ssh-aliases"
 
 OS_LIST="darwin linux"
-ARCH_LIST="386 amd64"
+ARCH_LIST="amd64 arm64"
 ADD_FILES="LICENSE README.md"
 
 mkdir -p ${OUT_DIR}
@@ -27,13 +25,14 @@ done
 for OS in ${OS_LIST}; do
     for ARCH in ${ARCH_LIST}; do
         echo "Making binary for ${OS}/${ARCH}"
-
         env CGO_ENABLED=0 GOOS=${OS} GOARCH=${ARCH} go build -a -o ${OUT_BINARY} -ldflags "-s -w -X main.Version=${VERSION}"
+
         if [ ${OS} == "darwin" ]; then
             zip -rj "${DIST}/ssh-aliases_${VERSION}_${OS}_${ARCH}.zip" ${OUT_DIR}
         else
             tar -C ${OUT_DIR} -czf "${DIST}/ssh-aliases_${VERSION}_${OS}_${ARCH}.tar.gz" .
         fi
+
         rm ${OUT_BINARY}
     done
 done
